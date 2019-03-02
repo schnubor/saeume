f89bcd81789589ae29c7496ba51767d0b1714533
schnubor <info@chko.org>
2019-03-02 13:13:24 +0100

    added nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){
-    $('.js-slick').slick({
-      dots : true,
-      arrows: false,
-      adaptiveHeight : true,
-      mobileFirst : true,
-    });
+$(document).ready(function () {
+  $('.js-slick').slick({
+    dots: true,
+    arrows: false,
+    adaptiveHeight: true,
+    mobileFirst: true,
   });
+
+  $('.navigation-wrapper .bars').click(function () {
+    showNav();
+  })
+
+  $('.js-close').click(function () {
+    hideNav();
+  })
+});
+
+function showNav() {
+  $('.nav-overlay').css("display", "flex").hide().fadeIn();
+}
+
+function hideNav() {
+  $('.nav-overlay').fadeOut();
+}
--- templates/navigation.php
+++ templates/navigation.php
@@ -17,3 +17,36 @@
         </div>
     </div>
 </div>
+
+<div class="nav-overlay">
+    <div class="close js-close">
+        <i class="fas fa-times"></i>
+    </div> 
+
+    <ul class="list">
+        <li class="item js-close">
+            <a href="/">Projekte</a>
+        </li>
+        <li class="item js-close">
+            <a href="/office">Büro</a>
+        </li>
+        <li class="item light js-close">
+            <a href="/office">Profil</a>
+        </li>
+        <li class="item light js-close">
+            <a href="/office">Portrait & Team</a>
+        </li>
+        <li class="item light js-close">
+            <a href="/office">Leistungen</a>
+        </li>
+        <li class="item light js-close">
+            <a href="/office">Technik</a>
+        </li>
+        <li class="item js-close">
+            <a href="/office">Kontakt</a>
+        </li>
+        <li class="item light js-close">
+            <a href="/impressum">Impressum</a>
+        </li>
+    </ul>
+</div>

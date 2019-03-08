82481523707f10bed838fdc02c0dfa766c1e4c15
schnubor <info@chko.org>
2019-03-08 19:46:07 +0100

    office content

--- inc/office.php
+++ inc/office.php
@@ -1,34 +1,69 @@
 <?php
-    try {
-        $entry = $contentfulClient->getEntry('4iBqsIVJm82DftUhiDlQXJ');
-    } catch (\Contentful\Core\Exception\NotFoundException $exception) {
-        debug_to_console( 'Contentful error: ' . $exception );
-    }
+try {
+    $entry = $contentfulClient->getEntry('4iBqsIVJm82DftUhiDlQXJ');
+} catch (\Contentful\Core\Exception\NotFoundException $exception) {
+    debug_to_console('Contentful error: ' . $exception);
+}
 
-    $renderer = new \Contentful\RichText\Renderer();
+$renderer = new \Contentful\RichText\Renderer();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php renderHead( 'Säum Architekten Berlin | Sanierung und Planung im denkmalgeschützten Bereich', 'TODO: Säum Architekten Berlin | Sanierung und Planung im denkmalgeschützten Bereich' ) ?>
+<?php renderHead('Säum Architekten Berlin | Sanierung und Planung im denkmalgeschützten Bereich', 'TODO: Säum Architekten Berlin | Sanierung und Planung im denkmalgeschützten Bereich') ?>
 
-    <body>
-        <?php include( TEMPLATE_DIR . '/navigation.php') ?>
+<body>
+    <?php include(TEMPLATE_DIR . '/navigation.php') ?>
 
-        <section id="office">
-            <div class="container">
-                <h2><?php echo $entry->title ?></h2>
-                <div class="row align-items-center">
-                    <div class="col-md-6">
-                        <?php echo nl2br($renderer->render($entry->profil)) ?>
-                    </div>
-                    <div class="col-md-6">
-                        <?php echo '<img src="' . $entry->profilbild->getFile()->getUrl() .'" />'; ?>
+    <section id="office">
+        <div class="container">
+            <h2><?php echo $entry->title ?></h2>
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <?php echo nl2br($renderer->render($entry->profil)) ?>
+                </div>
+                <div class="col-md-6">
+                    <?php echo '<img src="' . $entry->profilbild->getFile()->getUrl() . '" />'; ?>
+                </div>
+            </div>
+
+            <hr class="divider">
+
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <?php echo '<img src="' . $entry->portraitbild->getFile()->getUrl() . '" />'; ?>
+                </div>
+                <div class="col-md-6">
+                    <?php echo nl2br($renderer->render($entry->portrait)) ?>
+                </div>
+            </div>
+
+            <hr class="divider">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="list">
+                        <h3>Leistungen</h3>
+                        <?php echo nl2br($renderer->render($entry->leistungen)) ?>
                     </div>
                 </div>
             </div>
-        </section>
 
-        <?php include( TEMPLATE_DIR . '/footer.php') ?>
-    </body>
-</html>
+            <hr class="divider">
+
+            <div class="row">
+                <div class="col-md-6">
+                        <div class="list">
+                            <h3>Technik</h3>
+                            <?php echo nl2br($renderer->render($entry->technik)) ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include(TEMPLATE_DIR . '/footer.php') ?>
+</body>
+
+</html> 

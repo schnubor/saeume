b4623615cd95a8ee8a56e1ce3c2dbd7f7018e407
Christian Korndoerfer <schnuppser@gmail.com>
2019-04-08 00:00:43 +0200

    more projects is now responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,23 @@ $(document).ready(function () {
     arrows: false,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.navigation-wrapper .bars').click(function () {

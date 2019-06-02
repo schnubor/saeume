c2b1daa7bab81497b5c4d63808d64206e767daa3
schnubor <info@chko.org>
2019-06-03 00:44:52 +0200

    futura local

--- templates/head.php
+++ templates/head.php
@@ -9,5 +9,5 @@
     <link rel="stylesheet" href="/bower_components/slick-carousel/slick/slick.css">
     <link rel="stylesheet" href="/bower_components/slick-carousel/slick/slick-theme.css">
     <link rel="stylesheet" href="/assets/css/compiled.css">
-    <link href="https://fonts.googleapis.com/css?family=Hind:500|Open+Sans:300,400,700|Montserrat:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/webfont.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
